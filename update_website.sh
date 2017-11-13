43d04ac9b9680565e5d73d96ca16c5ce8e3ef7f7
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2017-11-13 19:48:23 +0100

    ajout corrections feuille exercices 2

--- update_website.sh
+++ update_website.sh
@@ -3,9 +3,9 @@ rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/BO_differentielles/*
 rsync -avz --delete ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/BO_differentielles/*pdf migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/BO_differentielles/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Revisions/*html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Revisions/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Revisions/*pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Revisions/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Complexes/Complexes.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Complexes/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Complexes/Complexes_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Complexes/
 
-# rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Complexes/
-# rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Complexes/Complexes_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Complexes/
 # rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
 # rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
 # rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
