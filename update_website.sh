d3e40a6bb4edc37c2aa4bd3ab7758a7d32ea07e9
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2017-11-22 19:21:18 +0100

    Mise à jour cours analyse de fonctions

--- update_website.sh
+++ update_website.sh
@@ -5,9 +5,9 @@ rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Revisions/*html  mig
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Revisions/*pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Revisions/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Complexes/Complexes.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Complexes/
 rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Complexes/Complexes_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Complexes/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2017/Calculus/Calculus.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Calculus/
+rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2017/Calculus/
 
-# rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
-# rsync -avz ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus/Calculus.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus/
 # rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.html  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
 # rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2_files  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
 # rsync -avz --delete-after ~/Documents/Cours/Cours/Licence_FdV/Calculus2016/Calculus_2/Calculus_2.pdf  migale:/projet/web/htdocs/migale/mmariadasso/public_html/Licence_FdV/2016/Calculus_2/
